35e073512675f7493a971cf9f7c6a857b9b7b688
kuba2901 <kubanenczak@gmail.com>
2025-10-26 22:35:59 +0100

    Document chatting

--- llamaindex-service/main.py
+++
@@ -1,7 +0,0 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-
-documents = SimpleDirectoryReader("data/proj_1").load_data()
-index = VectorStoreIndex.from_documents(documents)
-query_engine = index.as_query_engine()
-response = query_engine.query("Czego dotyczy ten dokument?")
-print(response)
