145f7856d0ae5cc1581cf2db6837347ee04e8b58
kuba2901 <kubanenczak@gmail.com>
2026-03-06 12:46:34 +0100

    feature(33)[Comprehensive logging via PLG Stack]: Configured python-side logger to use JSON format

--- backend/app/core/logger.py
+++ backend/app/core/logger.py
@@ -1,18 +1,15 @@
 import logging
-import sys
-
-def configure_logging(level: str = "INFO"):
-    formatter = logging.Formatter(
-        fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(formatter)
-
-    root = logging.getLogger()
-    root.setLevel(level)
-    root.addHandler(handler)
+from pythonjsonlogger.json import JsonFormatter
 
 def get_logger(name: str):
-    return logging.getLogger(name)
+    logger = logging.getLogger(name=name)
+    
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = JsonFormatter(
+            '%(asctime)s %(levelname)s %(name)s %(message)s %(module)s %(funcName)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    
+    return logger
