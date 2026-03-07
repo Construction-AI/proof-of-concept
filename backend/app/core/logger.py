a54dc2bdd0cfb245c3369109c3c34fb0db4d0f67
kuba2901 <kubanenczak@gmail.com>
2026-03-07 15:10:40 +0100

    feature(33)[Comprehensive logging via PLG Stack]: Provisioning basic dashboard

--- backend/app/core/logger.py
+++ backend/app/core/logger.py
@@ -5,23 +5,26 @@ import contextvars
 trace_id_var = contextvars.ContextVar("trace_id", default="SYSTEM")
 
 class TraceIdFilter(logging.Filter):
-    def filter(self, record): # type: ignore TODO: Specify Type
+    def filter(self, record):  # type: ignore
         record.trace_id = trace_id_var.get()
         return True
     
-def setup_global_logging():
-    formatter = JsonFormatter('%(asctime)s %(levelname)s %(name)s %(trace_id)s %(message)s')
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    handler.addFilter(TraceIdFilter())
-
-    for logger_name in ("uvicorn", "uvicorn.access", "uvicorn.error", "fastapi"):
-        sys_logger = logging.getLogger(logger_name)
-        sys_logger.handlers = [handler]
-        sys_logger.propagate = False
-
 def get_logger(name: str):
     logger = logging.getLogger(name)
+    
     if not logger.handlers:
-        pass 
+        formatter = JsonFormatter('%(asctime)s %(levelname)s %(name)s %(trace_id)s %(message)s')
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        handler.addFilter(TraceIdFilter())
+        
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+        logger.propagate = False 
+        
     return logger
+
+def setup_global_logging():
+    # Używamy tej samej logiki dla loggerów systemowych (FastAPI/Uvicorn)
+    for logger_name in ("uvicorn", "uvicorn.access", "uvicorn.error", "fastapi"):
+        get_logger(logger_name)
